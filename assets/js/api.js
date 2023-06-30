df781b7834bdf7c9291f9d54da45e3848a1942d8
Luiz Fernando <luiz.fpimentel0@gmail.com>
2023-06-29 21:41:25 -0300

    fix: corrigindo caminho do profile.json, agora vai

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
-  const url = '../../data/profile.json';
+  const url = 'data/profile.json';
   const response = await fetch(url);
+  console.log(response);
   return await response.json();
 }
